19bb003fc53831f8ecb92673c08a5789ebb34333
Sandesh-Bodukuri <0913sandesh@gmail.com>
2026-03-24 12:04:10 +0530

    task: taking input from user and printing it

---
+++ input.py
@@ -0,0 +1,19 @@
+val = input("Enter a number:")
+print(val)
+
+#finding the type of the input
+print(type(val))
+
+#typecasting the input to integer
+num = int(input("Enter a number:"))
+print("entered number is",num)
+print(type(num))
+#typecasting the input to float
+num1 = float(input("enter a number:"))
+print("entered number is",num1)
+print(type(num1))
+#taking multiple inputs
+x,y,z = input("enter the values of x,y,z using space:").split()
+print("value of x is:",x)
+print("value of y is:",y)
+print("value of z is:",z)
