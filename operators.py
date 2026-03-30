c4a67b9cdd5345abe0d20112e6fcddb7ab6ddbfa
Sandesh-Bodukuri <0913sandesh@gmail.com>
2026-03-30 21:45:57 +0530

    Arithmetic operators

---
+++ operators.py
@@ -0,0 +1,11 @@
+#Operators in python    
+a=69
+b=33
+#Arithmetic operators
+print("Addition:", a + b)  
+print("Subtraction:", a - b) 
+print("Multiplication:", a * b)  
+print("Division:", a / b) 
+print("Floor Division:", a // b)  
+print("Modulus:", a % b) 
+print("Exponentiation:", a ** b)
