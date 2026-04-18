c494dff660bc0dfbbc515e28fe4fda211b36ba4e
Sandesh-Bodukuri <0913sandesh@gmail.com>
2026-04-18 12:01:45 +0530

    tweaking variables and operators

---
+++ variables.py
@@ -0,0 +1,9 @@
+mass = int(input('enter the weight of the person in kg:'))
+height = float(input('enter the height of the person in meters:'))
+bmi = (mass/height**2)
+print('The BMI of the person is:',bmi)
+
+a = int(input('enter a:'))
+b = int(input('enter b:'))
+C = (a**2 + b**2)**0.5   #Finding c using pythagoreus theorem
+print('The value of c is:',C)
