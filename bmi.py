35fcdeb4306e5c66b655898ebe1203bc2bb2f481
Sandesh-Bodukuri <0913sandesh@gmail.com>
2026-04-18 11:51:55 +0530

    data types and operators

---
+++ bmi.py
@@ -0,0 +1,4 @@
+mass = int(input('enter the weight of the person in kg:'))
+height = float(input('enter the height of the person in meters:'))
+bmi = (mass/height**2)
+print('The BMI of the person is:',bmi)
