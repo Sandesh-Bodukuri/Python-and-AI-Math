2e86fe96c1df136dbc90861cf48d3231d2577136
Sandesh-Bodukuri <0913sandesh@gmail.com>
2026-06-21 21:45:37 +0530

    Update list_comp.py

--- list_comp.py
+++ list_comp.py
@@ -4,8 +4,8 @@ print(cubes)
 
 #list comprehension using condition
 even_cubes = [num**3 for num in range(0,11) if num % 2 ==0]
-print(even_cubes)
+print("The list of odd cubes from 0 to 10 :",even_cubes)
 odd_cubes = [num**3 for num in range(0,11) if num % 2 == 1]
-print(odd_cubes)
+print("The list of odd cubes from 0 to 10 :",odd_cubes)
 
 #using function in list comprehension
