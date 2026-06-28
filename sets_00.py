0602d2144d5828b5b63f35864e0bd1d7a32059f7
Sandesh-Bodukuri <0913sandesh@gmail.com>
2026-06-28 21:40:17 +0530

    refactoring

--- sets_00.py
+++ sets_00.py
@@ -1,9 +1,9 @@
 if __name__ == '__main__':
     # Read the number of elements (not strictly needed for the calculation)
-    n = int(input())
+    n = int(input("Enter the number of elements: "))
     
     # Read the space-separated scores and convert them into a list of integers
-    scores = list(map(int, input().split()))
+    scores = list(map(int, input("Enter each element with a space: ").split()))
     
     # 1. Remove duplicates by converting the list to a set
     unique_scores = set(scores)
@@ -12,4 +12,4 @@ if __name__ == '__main__':
     unique_scores.remove(max(unique_scores))
     
     # 3. The new maximum of the remaining elements is the runner-up score
-    print(max(unique_scores))
+    print(f"The runner up is : {max(unique_scores)}")
