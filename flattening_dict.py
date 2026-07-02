83e8244abf30d99c89a107306da60235fb171b15
Sandesh-Bodukuri <0913sandesh@gmail.com>
2026-07-02 21:13:15 +0530

    converting nested dict to single level dict

---
+++ flattening_dict.py
@@ -0,0 +1,18 @@
+def flatten_dict(nested_dict, parent_key=''):
+    flat_dict = {}
+    
+    for key, value in nested_dict.items():
+        # Build the new key with a dot separator
+        new_key = f"{parent_key}.{key}" if parent_key else key
+        
+        if isinstance(value, dict):
+            # If value is a dict, recurse
+            flat_dict.update(flatten_dict(value, new_key))
+        else:
+            # If value is a primitive, add to result
+            flat_dict[new_key] = value
+            
+    return flat_dict
+
+data = {"a": {"b": 1, "c": {"d": 2}}, "e": 3}
+print(flatten_dict(data))
