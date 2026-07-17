99c2414a4c8f574e8dc627bf8fde46dc0244605e
Sandesh-Bodukuri <0913sandesh@gmail.com>
2026-07-17 20:12:24 +0530

    implement librarie and modules

---
+++ mod.py
@@ -0,0 +1,15 @@
+import math
+
+radius = 5
+
+area = math.pi * (radius ** 2)
+
+print(f"Circle Area: {area:.2f}")
+
+
+from random import randint
+
+# Roll a 6-sided die
+dice_roll = randint(1, 6)
+
+print(f"You rolled a: {dice_roll}")
