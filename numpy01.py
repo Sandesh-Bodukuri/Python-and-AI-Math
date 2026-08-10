8e2446eeaca3cd0b5f33b42323ba611231d957b8
Sandesh-Bodukuri <0913sandesh@gmail.com>
2026-08-10 21:25:51 +0530

    implement numpy fundamentals

---
+++ numpy01.py
@@ -0,0 +1,10 @@
+import numpy as np
+one_dimensional_array = np.array([10,12])
+print(one_dimensional_array)
+k = np.array([[4,8,7],[5,6,9]])
+print(k)
+#Get Dimension
+print(one_dimensional_array.ndim)
+
+#Get Shape
+print(k.shape)
