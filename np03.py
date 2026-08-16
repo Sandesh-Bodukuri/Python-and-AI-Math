7ffbacb2d9668fe112e2efd8fda7846b96899437
Sandesh-Bodukuri <0913sandesh@gmail.com>
2026-08-16 06:34:48 +0530

    implement arithmetic operations with arrays

--- np03.py
+++ np03.py
@@ -5,3 +5,13 @@ print("Shape:", arr.shape)
 print("Dimensions:", arr.ndim)
 print("Data type:", arr.dtype)
 print("Total elements:", arr.size) 
+
+
+#arithmetic operations with arrays
+a = np.array([10, 20, 30])
+b = np.array([1, 2, 3])
+
+print("Addition:", a + b)       
+print("Multiplication:", a * b) 
+print("Scalar Math:", a * 2)    
+print("Exponent:", a ** 2)      
