2ce942f061578751e0df55d62cb948169d3e0aad
Sandesh-Bodukuri <0913sandesh@gmail.com>
2026-08-23 22:01:56 +0530

    implement numpy array creation methods

--- C_np.py
+++ C_np.py
@@ -13,3 +13,19 @@ print(d_int)
 #character array
 char_arr = np.array(['Welcome to Math for ML!'])
 print(char_arr)
+
+# Return a new array of shape 3, without initializing entries.
+empt_arr = np.empty(2, dtype=int)
+print(empt_arr)
+
+# Return a new array of shape 3 with random numbers between 0 and 1.
+rand_arr = np.random.rand(3)
+print(rand_arr)
+
+# Return a new array of shape 3, filled with zeroes.
+zeros_arr = np.zeros(3)
+print(zeros_arr)
+
+# Return a new array of shape 3, filled with ones. 
+ones_arr = np.ones(3)
+print(ones_arr)
